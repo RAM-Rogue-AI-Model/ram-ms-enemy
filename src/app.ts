2c1548f8710c540fa42391e3f0ba77b1dd6afb67
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 21:02:22 +0100

    openapi.yml

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 
+import fs from 'node:fs';
+
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yaml';
 
 import { config } from './utils/config';
 import { EnemyController } from './controllers/enemyController';
@@ -25,7 +29,13 @@ const enemyController = new EnemyController(enemyService);
 
 app.use('/enemy', new EnemyRouter(enemyController).router);
 
+const file = fs.readFileSync('./openapi.yml', 'utf8');
+const swaggerDocument = YAML.parse(file) as object;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`docs available at http://localhost:${port}/docs`);
 });
