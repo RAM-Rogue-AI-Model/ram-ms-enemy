d47bb992c30e76ee536247dacbe7d5ef9d9e6c01
FrancoisToureille <toureillefrancois@gmail.com>
2026-02-03 15:43:27 +0100

    ajout jeu de donnees

---
+++ dataset.sql
@@ -0,0 +1,6 @@
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'Graphiste', 30, 5, 10, 30);
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440001', 'Professeur', 50, 10, 8, 40);
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440002', 'Médecin', 80, 15, 5, 50);
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440003', 'ChatGpt', 70, 12, 11, 55);
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440004', 'Gemini', 90, 14, 10, 60);
+INSERT INTO Enemy (id, name, pv, attack, speed, probability_attack) VALUES ('550e8400-e29b-41d4-a716-446655440005', 'Mistral', 150, 20, 4, 65);
