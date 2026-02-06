93f1166243efd175bcb7ee6696d8614b620a9a76
FrancoisToureille <toureillefrancois@gmail.com>
2026-02-06 15:27:24 +0100

    Fixer

--- src/controllers/enemyController.ts
+++ src/controllers/enemyController.ts
@@ -17,7 +17,7 @@ class EnemyController {
         !body.pv ||
         !body.attack ||
         !body.speed ||
-        !body.probability_attack || 
+        !body.probability_attack ||
         body.is_boss === undefined
       ) {
         return res.status(400).json({ error: 'Missing required fields' });
